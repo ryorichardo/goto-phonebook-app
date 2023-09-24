a1239f0166974594cfcdf691b68be52dac82f119
ryorichardo <ryorichardo06@gmail.com>
2023-09-24 18:27:34 +0700

    fix: change ts version

--- src/__test__/FirstTest.test.jsx
+++ src/__test__/FirstTest.test.jsx
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react'
-import FirstTest from '../components/FirstTest';
+import { ContactCard } from '../views/components/ContactCard'
+
+const data = {
+    contact: {
+        id: 0,
+        first_name: 'contact',
+        last_name: 'last',
+        phones: []
+    },
+    setSelectedContact: null,
+    setDetailModal: null,
+    setDeleteModal: null,
+    setFavo: null
+}
+
 
 test("Example 1 renders successfully", () => {
-    render(<FirstTest/>);
-    const element = screen.getByText(/first test/i);
+    render(<ContactCard props={data} />);
+    const element = screen.getByText(/contact/i);
     expect(element).toBeInTheDocument();
 })
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,11 +4,3 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
-
-const FirstTest = () => {
-    return (
-      <div>
-          <h2> First test </h2>
-      </div>
-    )
-  }
